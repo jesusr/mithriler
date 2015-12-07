78dbca6b5b8cf82e44c70d144a98590b7aecdca8
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-12-07 14:06:49 +0100

    Initialize test server

---
+++ server.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+  var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 4443;
+  
+  mongoose.connect('mongodb://localhost:27017/colombia');
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.use(express.static(__dirname + '/dist'));
+  console.log(__dirname + '/dist');
+
+  app.get('*', function(req, res) {
+    res.sendfile('./dist/index.html');
+  });
+
+  app.listen(port);
+  console.log('Listening at ' + port);
+})();
+
